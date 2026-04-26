b14f20bd8d098c7a28e06d7ea2a8b9ba08f3a32b
hojinkim <ghwls0622@gmail.com>
2026-04-27 02:49:56 +0900

    fix: 운영 규칙 조회를 규칙 테이블 기준으로 제한

--- src/main/java/com/emailagent/service/admin/AdminCategoryKeywordService.java
+++ src/main/java/com/emailagent/service/admin/AdminCategoryKeywordService.java
@@ -87,7 +87,6 @@ public class AdminCategoryKeywordService {
 
         LinkedHashSet<String> categoryNames = new LinkedHashSet<>();
         categoryNames.addAll(rulesByName.keySet());
-        categoryNames.addAll(categoriesByName.keySet());
 
         return categoryNames.stream()
                 .map(categoryName -> toGroupedResponse(
