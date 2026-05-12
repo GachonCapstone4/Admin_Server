b3416f299716c372f02aecb805e1464408f66cae
박수한 <victor1919@naver.com>
2026-05-12 15:57:37 +0900

    컨슈머 작성 및 코드정리

--- src/main/java/com/emailagent/domain/enums/EmailStatus.java
+++ src/main/java/com/emailagent/domain/enums/EmailStatus.java
@@ -3,5 +3,6 @@ package com.emailagent.domain.enums;
 public enum EmailStatus {
     PENDING_REVIEW,   // 검토 대기
     PROCESSED,        // 처리 완료
-    AUTO_SENT         // 자동 발송됨
+    AUTO_SENT,        // 자동 발송됨
+    DELETED           // 삭제됨
 }
