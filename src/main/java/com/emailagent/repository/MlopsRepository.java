736681637d64dadf4b43a3c3521f37b006e995ba
박수한 <victor1919@naver.com>
2026-05-12 15:50:04 +0900

    컨슈머 작성 및 코드정리

--- src/main/java/com/emailagent/repository/MlopsRepository.java
+++ src/main/java/com/emailagent/repository/MlopsRepository.java
@@ -4,6 +4,6 @@ import com.emailagent.domain.entity.TrainingJob;
 import com.emailagent.domain.enums.TrainingJobStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrainingJobRepository extends JpaRepository<TrainingJob, String> {
+public interface MlopsRepository extends JpaRepository<TrainingJob, String> {
     long countByStatus(TrainingJobStatus status);
 }
